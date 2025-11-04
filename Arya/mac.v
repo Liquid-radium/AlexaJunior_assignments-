fe5538e8d09d75faa572417232737ce538582d18
shady-guy <moony.shady.o7@gmail.com>
2025-11-04 20:14:00 +0530

    added overflow check

--- Arya/mac.v
+++ Arya/mac.v
@@ -1,6 +1,17 @@
-module mac(input clk, input r, input [7:0] a, input [7:0] b, output reg [15:0] acc);
+module mac(input clk, input r, input [7:0] a, input [7:0] b, output reg [15:0] acc, output reg of);
+  	wire [16:0] c=acc+a*b;
   	always @(posedge clk or posedge r) begin
-      	if(r) acc<=0;
-		else acc<=acc+a*b;
+      	if(r) begin
+        	acc<=0;
+          	of<=0;
+        end
+      	else if(c[16]) begin
+        	acc<=16'hFFFF;
+          	of<=1;
+        end
+        else begin
+          	acc<=c[15:0];
+          	of<=0;
+        end
 	end
 endmodule
