c9aedb7b8f77b4cee5f32be7e0de1065c8d93341
shady-guy <moony.shady.o7@gmail.com>
2025-11-04 19:34:54 +0530

    Created mac.v

---
+++ Arya/mac.v
@@ -0,0 +1,7 @@
+module mac(input clk, input r, input [7:0] a, input [7:0] b, output reg [15:0] acc);
+  	wire [15:0] p=a*b;
+  	always @(posedge clk or posedge r) begin
+      	if(r) acc<=0;
+		    else acc<=acc+p;
+	  end
+endmodule
