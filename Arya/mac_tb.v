ae0610405979e4b36d837ed37af8a3d0bf60d340
shady-guy <moony.shady.o7@gmail.com>
2025-11-04 20:15:06 +0530

    added overflow testing

--- Arya/mac_tb.v
+++ Arya/mac_tb.v
@@ -1,3 +1,5 @@
+// Code your testbench here
+// or browse Examples
 `timescale 1ns/1ps
 module mac_tb;
     reg clk;
@@ -5,22 +7,25 @@ module mac_tb;
     reg [7:0] a;
     reg [7:0] b;
     wire [15:0] acc;
-	  mac uut(clk, r, a, b, acc);
+  	wire of;
+  	mac uut(clk, r, a, b, acc, of);
     initial begin
         clk = 0;
         forever #5 clk = ~clk; 
     end
   	initial begin
-      	$monitor("Time=%0t | r=%b | a=%d | b=%d | acc=%d", $time, r, a, b, acc);
+    $monitor("Time=%0t | r=%b | a=%d | b=%d | acc=%d | of=%b", $time, r, a, b, acc, of);
       	r=1; a=0; b=0;
-        #10; r=0;
+        #10;
+        r=0;
       
-      	a=6; b=7; #10;
+      	a=6; b=9; #10;
       	a=5; b=4; #10;
       	a=9; b=2; #10;
       	a=3; b=8; #10;
       	r=1; #10 r=0; 
-      	a=2; b=7; #10;
+      	a=255; b=255; #10;
+      	a=40; b=40; #10;
       	
       	$finish;
     end
