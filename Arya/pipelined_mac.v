7b4f38b6646e2a845164e26ae93b601f971f0324
shady-guy <moony.shady.o7@gmail.com>
2025-11-05 00:29:03 +0530

    added comments

--- Arya/pipelined_mac.v
+++ Arya/pipelined_mac.v
@@ -1,5 +1,6 @@
 module pipelined_mac(input clk, input r, input [7:0] a, input [7:0] b, output reg [15:0] acc, output reg of);
-  
+
+	// Stage 1 - inputs
   	reg [7:0] A;
   	reg [7:0] B;
   	always@(posedge clk or posedge r) begin
@@ -12,21 +13,23 @@ module pipelined_mac(input clk, input r, input [7:0] a, input [7:0] b, output re
             B<=b;
         end
   	end
-  
+
+	// Stage 2 - product
   	reg [15:0] P;
   	always@(posedge clk or posedge r) begin
     	if(r) P<=0;
     	else P<=A*B;
   	end
-  
+
+	// Stage 3 - accumulator
   	wire [16:0] c=acc+P;
   	always @(posedge clk or posedge r) begin
       	if(r) begin
-        	  acc<=0;
+        	acc<=0;
           	of<=0;
         end
       	else if(c[16]) begin
-        	  acc<=16'hFFFF;
+        	acc<=16'hFFFF;
           	of<=1;
         end
         else begin
